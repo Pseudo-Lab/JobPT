49840bac0df437b2e8b6c643cf8414313d1ed663
workdd <workdd@gmail.com>
2024-11-25 13:07:30 +0000

    add promptfoo files

--- news_summary/promptfoo/generate_promptfoo_config.py
+++ news_summary/promptfoo/generate_promptfoo_config.py
@@ -1,5 +1,25 @@
 import yaml
 import json
+import pandas as pd
+
+# 데이터 로드 (예: CSV 파일로부터 읽기)
+file_path = "news_data.csv"  # CSV 파일 경로
+data = pd.read_csv(file_path)
+
+# 데이터 병합: company_name을 기준으로 병합
+merged_data = (
+    data.groupby("company_name")
+    .agg(
+        {
+            "published_date": lambda x: ", ".join(x),
+            "title": lambda x: "\n".join(x),
+            "description": lambda x: "\n---\n".join(x),
+            "url": lambda x: ", ".join(x),
+            "origin": lambda x: ", ".join(x),
+        }
+    )
+    .reset_index()
+)
 
 
 def load_text_from_file(file_path):
@@ -15,7 +35,8 @@ def load_test_data_from_json(file_path):
 
 
 base_path = "prompts/"
-prompt_files = ["v5.txt", "v7.txt"]
+# prompt_files = ["v5.txt", "v7.txt"]
+prompt_files = ["final.txt"]
 
 # 프롬프트 파일들을 불러와서 리스트에 추가
 prompts = []
@@ -25,26 +46,29 @@ for file_path in prompt_files:
 
 
 # 테스트 케이스용 텍스트 파일 경로 설정
-test_data = load_test_data_from_json("answer.json")
+# test_data = load_test_data_from_json("answer.json")
 
 # 테스트 케이스 생성
 tests = []
-for idx, item in enumerate(test_data):
-    user_text = item["article"]
-    assistant_text = item["summary"]
+for idx, description in enumerate(merged_data["description"]):
+    print(description)
+    user_text = description
     test_case = {
         "vars": {"text": user_text},
         "assert": [
             # Chat GPT API 필요
-            {"type": "llm-rubric", "value": f"Ensure the [article] captures the main ideas accurately and concisely.\n [article]\n{user_text}"},
+            {
+                "type": "llm-rubric",
+                "value": f"Ensure the [article] captures the main ideas accurately and concisely.\n [article]\n{user_text}",
+                "provider": "openai:gpt-4o-mini",
+            },
             # {"type": "rouge-n", "value": assistant_text, "threshold": 0.4},
         ],
     }
     tests.append(test_case)
-    if idx == 14:
-        break
 
-providers = ["file://inference.py:generate_text_qwen", "file://inference.py:generate_text_qwen_templow"]
+providers = ["file://inference.py:generate_text_qwen_templow"]
+# providers = ["file://inference.py:generate_text_qwen", "file://inference.py:generate_text_qwen_templow"]
 # YAML 구성 설정
 config = {
     "description": "News Summary Evaluation",
