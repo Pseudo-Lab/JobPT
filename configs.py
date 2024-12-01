1b8c77329f22624cf0136e192352d6c3f26dc4d5
minglet <minah@tag-hive.com>
2024-12-01 17:13:49 +0000

    update configs

--- configs.py
+++ configs.py
@@ -2,4 +2,7 @@ import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "") 
 RAG_MODEL = os.getenv("RAG_MODEL", "gpt-4o-mini") #default: gpt-4o-mini
+DB_PATH = './chroma_db'
 PROMPT = './resume_JD_similarity/data/prompt.yaml'
+JD_PATH = './resume_JD_similarity/data/jd_origin'
+COLLECTION = 'semantic_0'
