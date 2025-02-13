a7b936ceea5733e58ec8c6f9c9fbac0a73ae7ef1
minglet <minah@tag-hive.com>
2025-02-13 16:33:35 +0000

    update jd matching prompt

--- system/configs.py
+++ system/configs.py
@@ -3,6 +3,7 @@ import os
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 RAG_MODEL = os.getenv("RAG_MODEL", "gpt-4o-mini")  # default: gpt-4o-mini
 DB_PATH = "./chroma_db"
-PROMPT = "system/get_similarity/data/prompt.yaml"
+PROMPT_YAML = "system/get_similarity/data/prompt.yaml"
+JD_MATCH_PROMPT = 'prompt_template_1'
 JD_PATH = "./data/jd_origin"
 COLLECTION = "semantic_0"
