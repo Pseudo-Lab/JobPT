cb74df9ce43d245cfe22bc5e7de709279c1ce751
최재강 <workd.official@gmail.com>
2025-03-13 10:48:34 +0900

    update req txt

--- system/configs.py
+++ system/configs.py
@@ -2,8 +2,8 @@ import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 RAG_MODEL = os.getenv("RAG_MODEL", "gpt-4o-mini")  # default: gpt-4o-mini
-DB_PATH = "/app/chroma_db"
+DB_PATH = "chroma_db"
 PROMPT_YAML = "system/get_similarity/data/prompt.yaml"
 JD_MATCH_PROMPT = "prompt_template_1"
-JD_PATH = "/app/data/jd_origin"
+JD_PATH = "data/jd_origin"
 COLLECTION = "semantic_0"
